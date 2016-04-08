6131a4bce88c3d4af0d8a8256d1aba13c5560099
chocolateSundae <xiu@xiu.local>
2016-04-08 20:15:05 +0800

    Login 完善－手动登陆
    由于云信  RequestCallBack 判断登陆成功 有延时 所以事实上只从LeanCloud中读取Token 然后直接进入主页面
    没有判断云信登陆是否成功

--- app/src/main/java/com/example/xiu/im0/LoginActivity.java
+++ app/src/main/java/com/example/xiu/im0/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.xiu.im0;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -9,16 +13,42 @@ import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle bundle = msg.getData();
+            if(bundle.getBoolean("isLoginSuccess")){
+//                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                System.out.println("Finally loginSucceed");
+                String token = bundle.getString("token");
+                String userName = bundle.getString("userName");
+                editor.putString("userName",userName);
+                editor.putString("token", token);
+                editor.commit();
+
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            }else{
+                System.out.println("Finally loginFailed");
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        sharedPreferences = getSharedPreferences("user",MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
         final EditText username = (EditText)findViewById(R.id.username);
         final EditText userpwd = (EditText)findViewById(R.id.userpwd);
         final Button signin = (Button)findViewById(R.id.signin);
         final Button login = (Button)findViewById(R.id.login);
+
 
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,7 +69,7 @@ public class LoginActivity extends AppCompatActivity {
                 String usernameS = username.getText().toString().toLowerCase();
                 String userpwdS = userpwd.getText().toString().toLowerCase();
 
-                LoginThread loginThread = new LoginThread(usernameS,userpwdS);
+                LoginThread loginThread = new LoginThread(usernameS,userpwdS,handler);
                 loginThread.start();
             }
         });
--- app/src/main/java/com/example/xiu/im0/LoginThread.java
+++ app/src/main/java/com/example/xiu/im0/LoginThread.java
@@ -1,9 +1,16 @@
 package com.example.xiu.im0;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
-import com.avos.avoscloud.FindCallback;
+import com.netease.nimlib.sdk.NIMClient;
+import com.netease.nimlib.sdk.RequestCallback;
+import com.netease.nimlib.sdk.auth.AuthService;
+import com.netease.nimlib.sdk.auth.LoginInfo;
 
 import java.util.List;
 
@@ -14,29 +21,84 @@ public class LoginThread extends Thread{
 
     private String userName;
     private String userPwd;
-    private String token;
+    private Handler handler;
 
-    public LoginThread(String userName,String userPwd){
+    public LoginThread(String userName,String userPwd ,Handler handler){
         this.userName = userName;
         this.userPwd = userPwd;
+        this.handler = handler;
     }
 
-    @Override
-    public void run() {
+    private String token ;
+    public String leanCloudLogin(String userName,String userPwd){
+
         AVQuery<AVObject> query = new AVQuery<AVObject>("UserInfo");
         query.whereEqualTo("userName", userName);
         query.whereEqualTo("userPwd", userPwd);
-        query.findInBackground(new FindCallback<AVObject>() {
-            @Override
-            public void done(List<AVObject> list, AVException e) {
-                if(list.isEmpty()){
-                    System.out.println("用户名密码错误");
-                }else {
-                    AVObject userInfo = list.get(0);
-                    token = userInfo.getString("token");
-                    System.out.println("登录成功 token:" + token);
-                }
-            }
-        });
+        try {
+            List<AVObject> avObjects = query.find();
+            AVObject userInfo = avObjects.get(0);
+            token = userInfo.getString("token");
+            System.out.println("登录成功 token:" + token);
+        } catch (AVException e) {
+            System.out.println("用户名密码错误");
+            token = null;
+            e.printStackTrace();
+        }
+        return token;
+    }
+
+
+//
+
+    public void NIMLogin(final String userName, final String token) {
+        LoginInfo info = new LoginInfo(userName,token); // config...
+        RequestCallback<LoginInfo> callback =
+                new RequestCallback<LoginInfo>() {
+                    // 可以在此保存LoginInfo到本地，下次启动APP做自动登录用
+
+                    @Override
+                    public void onSuccess(LoginInfo loginInfo) {
+                        System.out.println("NIMLogin:success");
+                    }
+
+                    @Override
+                    public void onFailed(int i) {
+                        System.out.println("NIMLogin:failed");
+                    }
+
+                    @Override
+                    public void onException(Throwable throwable) {
+                        System.out.println("NIMLogin:exception");
+                    }
+                };
+        NIMClient.getService(AuthService.class).login(info)
+                .setCallback(callback);
+    }
+
+
+    public void sendMessageBack(boolean isLoginSuccess,String userName,String token){
+        Message msg = new Message();
+        Bundle bundle = new Bundle();
+        bundle.putBoolean("isLoginSuccess",isLoginSuccess);
+        bundle.putString("token", token);
+        bundle.putString("userName",userName);
+        msg.setData(bundle);
+        handler.sendMessage(msg);
+    }
+
+    boolean isLoginSuccess;
+    @Override
+    public void run() {
+        token = leanCloudLogin(userName,userPwd);
+        if (token.equals(null)){
+            isLoginSuccess =  false;
+            System.out.println("leanCloudLogin false");
+        }else {
+            System.out.println("leanCloudLogin success");
+            NIMLogin(userName,token);
+            isLoginSuccess = true;
+        }
+        sendMessageBack(isLoginSuccess,userName,token);
     }
 }
